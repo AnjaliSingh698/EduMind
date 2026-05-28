d6f373748a182f3f58df2405398d67a2b98b8b28
AnjaliSingh698 <anjalisinghh6295@gmail.com>
2026-05-28 17:50:54 +0530

    conneting backend to frontend

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -17,7 +17,8 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://anjalisingh698.github.io"
+    "https://anjalisingh698.github.io",
+    "https://anjalisingh698.github.io/quiz"
   ],
   credentials: true
 }));
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 import axios from "axios";
+import API from "../API";
 
 const Login = () => {
   const navigate = useNavigate();
